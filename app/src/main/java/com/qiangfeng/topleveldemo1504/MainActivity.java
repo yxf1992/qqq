1d2e9021f3a9ead3a12ec8b035331a3f53c31ea2
LENOVO <2498282880@qq.com>
2016-03-14 11:56:16 +0800

    新增的TabLayout的使用其中包含结合veiwpager的使用步骤

--- app/src/main/java/com/qiangfeng/topleveldemo1504/MainActivity.java
+++ app/src/main/java/com/qiangfeng/topleveldemo1504/MainActivity.java
@@ -1,13 +1,22 @@
 package com.qiangfeng.topleveldemo1504;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mContentPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTabLayout = (TabLayout) findViewById(R.id.main_tab_layout);
+        mContentPager = (ViewPager) findViewById(R.id.main_content_view_page);
+        //给tabLayout添加Tab按钮
+//        mTabLayout.addTab(mTabLayout.newTab().setText(""));
     }
 }
